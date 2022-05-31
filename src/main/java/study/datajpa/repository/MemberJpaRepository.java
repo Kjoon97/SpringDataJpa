4e15df87c7a8e0e59c71c9e8c2f9982f972f93d1
joonhyuck <jh83370@naver.com>
2022-05-31 18:29:53 +0900

    순수 JPA기반 리포지토리 만들기

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -12,11 +15,34 @@ public class MemberJpaRepository {
     @PersistenceContext
     private EntityManager em;
 
+    //create
     public Member save(Member member){
         em.persist(member);
         return member;
     }
 
+    //delete
+    public void delete(Member member){
+        em.remove(member);
+    }
+
+    //전체 조회
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    //Optional- null일수도 있고 아닐 수도 있고.
+    public Optional<Member> findById(Long id){
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    //회원 수 조회.
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
+
+    //멤버 조회
     public Member find(Long id){
         return em.find(Member.class, id);
     }

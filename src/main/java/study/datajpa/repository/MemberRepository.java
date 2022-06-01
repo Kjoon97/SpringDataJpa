e257939cf09ada31a064875bb670ea01bb30836d
joonhyuck <jh83370@naver.com>
2022-06-01 16:55:16 +0900

    @Query, 리포지토리 메소드에 쿼리 정의하기

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -15,7 +15,11 @@ public interface MemberRepository extends JpaRepository<Member,Long> {  // <Memb
     //By뒤에 조건이 없으면 전부 조회. , Top3는 3개 조회.
     List<Member> findTop3HelloBy();
 
-    //스프링 데이터 jpa-  네임드쿼리 사용하기.
+    //스프링 데이터 jpa-  네임드쿼리 사용하기.(실무에서 잘 사용 안함.)
     @Query(name = "Member.findByUsername")
     List<Member> findByUsername(@Param("username") String username);
+
+    //메서드에 JPQL 쿼리 작성(이 방법을 실무에서 사용 많이 함.)
+    @Query("select m from Member m where m.username= :username and m.age = :age")  // -> 오타나면 컴파일 에러 뜸.
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
